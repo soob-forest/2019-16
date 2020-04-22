f21b01aff2973a379e553510fe96f643a4a7be87
soob-forest <dltntnqo@naver.com>
2020-04-22 16:05:17 +0900

    Fix : url을 restful하게 변경
    url으로 요청 resource를 http method로 행위를 표현

--- server/api-gateway/routes/api/search/index.js
+++ server/api-gateway/routes/api/search/index.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const { makePacket } = require("../../../../lib/tcp/util");
 
-module.exports = function(apiGateway) {
-  router.get("/suggest/:searchWord", (req, res, next) => {
+module.exports = function (apiGateway) {
+  router.get("studygroups/:searchWord/suggest", (req, res, next) => {
     const { searchWord } = req.params;
 
     req.packet = makePacket(
@@ -22,82 +22,89 @@ module.exports = function(apiGateway) {
   });
 
   router.get(
-    "/query/:searchWord/location/:lat/:lon/page/:page/:isRecruit",
+    "/studygroups/:searchWord/mode/:mode/location/:lat/:lon/page/:page/:isRecruit",
     (req, res, next) => {
-      const { searchWord, lat, lon, page, isRecruit } = req.params;
-
-      req.packet = makePacket(
-        "POST",
-        "apigateway",
-        "searchStudyGroup",
-        "searchStudyGroup",
-        { searchWord, lat, lon, page, isRecruit },
-        {},
-        req.resKey,
-        apiGateway.context
-      );
+      const { searchWord, mode, lat, lon, page, isRecruit } = req.params;
+      let packet;
+      if (mode === "normal") {
+        packet = makePacket(
+          "GET",
+          "apigateway",
+          "searchStudyGroup",
+          "searchStudyGroup",
+          { searchWord, lat, lon, page, isRecruit },
+          {},
+          req.resKey,
+          apiGateway.context
+        );
+      }
+      if (mode === "tag") {
+        packet = makePacket(
+          "GET",
+          "apigateway",
+          "tagStudyGroup",
+          "tagStudyGroup",
+          { tags, lat, lon, page, isRecruit },
+          {},
+          req.resKey,
+          apiGateway.context
+        );
+      }
+      req.packet = packet;
       next();
     }
   );
 
   router.get(
-    "/query/:searchWord/category/:category/location/:lat/:lon/page/:page/:isRecruit",
+    "/studygroups/:searchWord/mode/:mode/category/:category/location/:lat/:lon/page/:page/:isRecruit",
     (req, res, next) => {
-      const { searchWord, category, lat, lon, page, isRecruit } = req.params;
-
-      req.packet = makePacket(
-        "POST",
-        "apigateway",
-        "searchStudyGroupWithCategory",
-        "searchStudyGroupWithCategory",
-        { searchWord, category, lat, lon, page, isRecruit },
-        {},
-        req.resKey,
-        apiGateway.context
-      );
+      const {
+        searchWord,
+        mode,
+        category,
+        lat,
+        lon,
+        page,
+        isRecruit
+      } = req.params;
+      let packet;
+      if (mode === "normal") {
+        packet = makePacket(
+          "GET",
+          "apigateway",
+          "searchStudyGroupWithCategory",
+          "searchStudyGroupWithCategory",
+          { searchWord, category, lat, lon, page, isRecruit },
+          {},
+          req.resKey,
+          apiGateway.context
+        );
+      }
+      if (mode === "tag") {
+        packet = makePacket(
+          "GET",
+          "apigateway",
+          "tagStudyGroupWithCategory",
+          "tagStudyGroupWithCategory",
+          { tags, isRecruit, lat, lon, page, category },
+          {},
+          req.resKey,
+          apiGateway.context
+        );
+      }
+      req.packet = packet;
 
       next();
     }
   );
 
-  router.post("/tags/page/:page", (req, res, next) => {
-    const page = req.params.page;
-    const { tags, category, lat, lon, isRecruit } = req.body;
-
-    if (category === undefined)
-      req.packet = makePacket(
-        "POST",
-        "apigateway",
-        "tagStudyGroup",
-        "tagStudyGroup",
-        { tags, lat, lon, page, isRecruit },
-        {},
-        req.resKey,
-        apiGateway.context
-      );
-
-    if (category !== undefined)
-      req.packet = makePacket(
-        "POST",
-        "apigateway",
-        "tagStudyGroupWithCategory",
-        "tagStudyGroupWithCategory",
-        { tags, isRecruit, lat, lon, page, category },
-        {},
-        req.resKey,
-        apiGateway.context
-      );
-
-    next();
-  });
-
   router.get(
-    "/all/location/:lat/:lon/page/:page/:isRecruit",
+    "/studygroups/location/:lat/:lon/page/:page/:isRecruit",
     (req, res, next) => {
       const { lat, lon, page, isRecruit } = req.params;
 
       req.packet = makePacket(
-        "POST",
+        "GET",
         "apigateway",
         "searchAllStudyGroup",
         "searchAllStudyGroup",
@@ -112,12 +119,12 @@ module.exports = function(apiGateway) {
   );
 
   router.get(
-    "/all/category/:category/location/:lat/:lon/page/:page/:isRecruit",
+    "/studygroups/category/:category/location/:lat/:lon/page/:page/:isRecruit",
     (req, res, next) => {
       const { category, lat, lon, page, isRecruit } = req.params;
 
       req.packet = makePacket(
-        "POST",
+        "GET",
         "apigateway",
         "searchAllStudyGroupWithCategory",
         "searchAllStudyGroupWithCategory",
