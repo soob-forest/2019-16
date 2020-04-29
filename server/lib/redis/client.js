b6d0d3b92a9d8d817f8777bdb71fa339a9036179
sooob <dltntnqo@naver.com>
2020-04-30 01:50:28 +0900

    fix: reids host, port 하드코딩 수정

--- server/lib/redis/client.js
+++ server/lib/redis/client.js
@@ -1,15 +1,15 @@
-const { CACHE_HOST } = process.env;
+const { CACHE_HOST,CACHE_PORT, QUEUE_PORT, QUEUE_HOST } = process.env;
 
 let Redis = require("ioredis");
 
 const redis = require("redis");
 
-const cacheClient = redis.createClient(6379, CACHE_HOST);
+const cacheClient = redis.createClient(CACHE_PORT, CACHE_HOST);
 
 const client = new Redis({
   sentinels: [
-    { host: "106.10.41.25", port: 16379 },
-    { host: "106.10.41.25", port: 16378 }
+    { host: QUEUE_HOST, port: QUEUE_PORT },
+    { host: QUEUE_HOST, port: QUEUE_PORT }
   ],
   name: "mymaster"
 });
